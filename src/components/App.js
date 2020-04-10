ae5716d2e45f30f9b850cd65900aa1baa95d2ce2
Gurung, Abhinav K <abhinavkumar.gurung@cloud.statcan.ca>
2020-04-10 22:15:29 +0000

    adding some missing data

--- src/components/App.js
+++ src/components/App.js
@@ -200,7 +200,7 @@ class App extends Component {
         return (
             <div className={`App ${darkMode ? 'dark' : ''}`}>
                 <div className="jumbotron">
-                <p>This is an example visualization. The information displayed may be out-of-date or inaccurate.</p>
+                    <p>This is an example visualization. The information displayed may be out-of-date or inaccurate.</p>
                 </div>
                 <Helmet>
                     <title>{i18n.COVID19[lang]}</title>
